6cd65c440faf37d316842bc3999b7276ddacd77c
Lord World <mr.pro100y.patsan@mail.ru>
2024-04-11 22:10:37 +0700

    done

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -9,12 +9,66 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class MainClass {
+    WebDriver driver;
+    public MainClass(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mrpro\\Downloads\\web_9\\drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
 
     @Test
-    public void FirstTest() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mrpro\\Downloads\\web_9\\drivers\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+    public void Habr(){
+        //Переходим на Habr.com
+        driver.get("https://habr.com/ru/articles/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
+        //Нажимаем на "Поиск"
+        driver.findElement(
+                By.xpath("//*[@class=\"tm-svg-img tm-header-user-menu__icon tm-header-user-menu__icon_search\"]")
+        ).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        //Проверяем, является ли поле ввода запроса активным элементом
+        Assert.assertEquals(
+                driver.findElement(
+                        By.xpath("//*[@class=\"tm-input-text-decorated__input\"]")
+                ),
+                driver.switchTo().activeElement()
+        );
+
+        //Отправляем поисковой запрос
+        driver.findElement(By.xpath("//*[@class=\"tm-input-text-decorated__input\"]"))
+                .sendKeys("Selenium WebDriver");
+        driver.findElement(By.xpath("//*[@class=\"tm-svg-img tm-svg-icon\"]"))
+                .click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        //Открываем статью из результатов поиска
+        driver.findElement(By.linkText("Что такое Selenium?"))
+                .click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        //Проверяем дату на соответствие
+        Assert.assertEquals(
+                "28 сен 2012 в 17:14",
+                driver.findElement(
+                        By.xpath("//*[@title=\"2012-09-28, 17:14\"]")
+                ).getText()
+        );
+
+        //Пролистываем вниз страницы
+        JavascriptExecutor js = ((JavascriptExecutor) driver);
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        //Меняем размер окна и нажимаем на раздел "Статьи"
+        driver.manage().window().setSize(new Dimension(1280, 1025));
+        driver.findElement(
+                By.xpath("//*[@href=\"/ru/articles/\" and @class=\"footer-menu__item-link router-link-active\"]")
+        ).click();
+    }
+
+    @Test
+    public void MailRu() {
         //Переходим на Mail.ru
         driver.get("https://account.mail.ru/login");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
@@ -58,7 +112,7 @@ public class MainClass {
 
         //Проверяем наличие кнопки регистрации
         driver.findElement(
-                By.xpath("//*[@class=\"resplash-btn resplash-btn_outlined-themed resplash-btn_mailbox-big cea-fefe-ehxscg\"]")
+                By.xpath("//*[@class=\"resplash-btn resplash-btn_outlined-themed resplash-btn_mailbox-big ejf-iekc__1jdtl7f\"]")
         ).isDisplayed();
 
         driver.quit();
